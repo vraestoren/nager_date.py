203171f719c658abd24b4fd2b588a3d4ae3d22fc
huok <77536370+vraestoren@users.noreply.github.com>
2026-04-22 15:19:56 +0300

    Update nager_date.py

--- src/nager_date.py
+++ src/nager_date.py
@@ -1,4 +1,4 @@
-from requests import Session
+from requests import Session, Response
 
 class NagerDate:
 	def __init__(self) -> None:
@@ -7,36 +7,35 @@ class NagerDate:
 		self.session.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
 		}
-	
+
+	def _get(self, endpoint: str) -> Response:
+		return self.session.get(f"{self.api}{endpoint}")
+
 	def get_country_info(self, county_code: str) -> dict:
-		return self.session.get(
-			f"{self.api}/CountryInfo/{country_code}").json()
+		return self._get(f"/CountryInfo/{country_code}").json()
 	
 	def get_available_countries(self) -> dict:
-		return self.session.get(f"{self.api}/AvailableCountries").json()
+		return self._get("/AvailableCountries").json()
 	
 	def get_long_weekends(
 			self,
 			year: int,
 			country_code: str) -> dict:
-		return self.session.get(
-			f"{self.api}/LongWeekend/{year}/{country_code}").json()
+		return self._get(f"/LongWeekend/{year}/{country_code}").json()
 	
 	def get_public_holidays(
-			self,
-			year: int,
-			country_code: str) -> dict:
-		return self.session.get(
-			f"{self.api}/PublicHolidays/{year}/{country_code}").json()
+			self, year: int, country_code: str) -> dict:
+		return self._get(
+			f"/PublicHolidays/{year}/{country_code}").json()
 	
 	def is_today_public_holiday(self, country_code: str) -> int:
-		return self.session.get(
-			f"{self.api}/IsTodayPublicHoliday/{country_code}").status_code
+		return self._get(
+			f"/IsTodayPublicHoliday/{country_code}").status_code
 	
 	def get_upcoming_public_holidays(self, country_code: str) -> dict:
-		return self.session.get(
-			f"{self.api}/NextPublicHolidays/{country_code}").json()
+		return self._get(
+			f"/NextPublicHolidays/{country_code}").json()
 	
 	def get_worldwide_upcoming_public_holidays(self) -> dict:
-		return self.session.get(
-			f"{self.api}/NextPublicHolidaysWorldWide").json()
+		return self._get(
+			f"/NextPublicHolidaysWorldWide").json()
